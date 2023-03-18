ab229ad699e9e30ca8b2c066b5d98c33ec64775f
AndresBochi <elmaildeandresbochi@gmail.com>
2023-03-18 02:49:27 -0300

    Agregados Formularios Creación y Edición de Productos - productCreateForm.ejs y productEditForm.ejs - productForm.css

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -8,14 +8,23 @@ const productsController = {
     },
 
     detalle: (req, res) => {
-        // En req.params tenemos el parametro que definimos en el enrutador (en este caso con la palabra num)
+        // En req.params tenemos el parametro que definimos en el enrutador 
         let productoId = req.params.numeroProducto;
         res.render("products/productDetail"+productoId);
     },
 
      // Procesa el pedido get con ruta /carrito
-     carrito: (req, res) => {
+    carrito: (req, res) => {
         res.render("products/productCart");
+    },
+
+    crear: (req, res) => {
+        res.render("products/productCreateForm");
+    },
+
+    editar: (req, res) => {
+        let productoId = req.params.numeroProducto;
+        res.render("products/productEditForm"+productoId);
     },
 
 }
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -8,10 +8,16 @@ const productsController = require("../controllers/productsController.js")
 
 // En vez de app.get, utilizamos router.get. Esto va "guardando" en router las distintas rutas, que luego exportamos
 
-// Ruta parametrizada!!
+// Listado de todos los productos
 router.get("/", productsController.index);
+// Carrito de compras
 router.get("/carrito", productsController.carrito);
+// Dar de alta un producto
+router.get("/crear", productsController.crear);
+// Detalle de un producto
 router.get("/detalle/:numeroProducto", productsController.detalle);
+// Editar un producto
+router.get("/editar/:numeroProducto", productsController.editar);
 
 // Exportamos la variable router ya con todas las rutas "guardadas", que se usará en app.js
 module.exports = router;
