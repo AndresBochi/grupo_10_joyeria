05ab5d990a6c90b5174f56b5c6e6e3921afdc758
stefania-tm88 <stefania.tm88@gmail.com>
2023-04-19 20:54:40 -0300

    Comienzo implementacion Multer

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -3,6 +3,22 @@ const express = require("express");
 // Router con R mayuscula
 const router = express.Router();
 
+// Multer (para cargar archivos)
+
+const multer = require('multer'); 
+const path = require('path')
+
+const storage= multer.diskStorage({
+    destination : function (req, file, cb){
+        cb(null, path.join(__dirname, '../../public/images/products'));
+    },
+    filename: function (req, file, cb){
+        cb(null, '${Date.now()}_img_${path.extname(file.originalname)}');
+    }
+});
+
+const uploadFile = multer ({storage});
+
 // Importamos el controlador de productos
 const productsController = require("../controllers/productsController.js")
 
