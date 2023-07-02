b3db7af925e3168a415789e59abca3f7cbe3ed50
stefania-tm88 <stefania.tm88@gmail.com>
2023-07-02 03:36:43 +0200

    cambios en archivo app

--- src/app.js
+++ src/app.js
@@ -2,16 +2,14 @@
 const express = require('express'); // Importamos modulo express
 const path = require('path'); // Importamos modulo nativo path y lo guardamos en la constante path
 const methodOverride = require('method-override'); // Para poder usar los métodos PUT y DELETE
-<<<<<<< HEAD
+
 const session = require('express-session'); //Requiero el paquete session
-<<<<<<< HEAD
+
 const userLoggedMiddleware = require('./middlewares/userLoggedMiddleware');//************/
 const cookies = require('cookie-parser');//************************** */
-=======
-=======
+
 const session = require ('express-session');
->>>>>>> 5d87cf3d72eabde44d4e43318d214ac32cc83019
->>>>>>> 14b4eec09a15e3f34d4f3f4d43e4c3b6010ccc29
+
 
 // ** express() **
 const app = express(); // Instanciamos Express y lo asignamos a la constante app
@@ -28,17 +26,12 @@ app.use(express.static(path.join(__dirname, '../public'))); // Habilitamos la ca
 app.use(express.urlencoded({ extended: false })); // Para poder leer el body de los formularios
 app.use(express.json());
 app.use(methodOverride('_method')); // Para poder usar los métodos PUT y DELETE
-<<<<<<< HEAD
-app.use(session({secret: "Nuestro mensaje secreto", //El texto secreto puede ser cualquier texto aleatorio.
-                resave: false,
-	            saveUninitialized: false,}));
-=======
-<<<<<<< HEAD
-app.use(session({secret: "Nuestro mensaje secreto"})); //El texto secreto puede ser cualquier texto aleatorio.
-=======
-app.use(session( {secret: "Anastassia Joyas"} ));
->>>>>>> 5d87cf3d72eabde44d4e43318d214ac32cc83019
->>>>>>> 14b4eec09a15e3f34d4f3f4d43e4c3b6010ccc29
+
+
+app.use(session( {secret: "Anastassia Joyas",//El texto secreto puede ser cualquier texto aleatorio.
+                 resave: false,
+	             saveUninitialized: false,} ));
+
 
 // ** Rutas **
 
@@ -53,13 +46,6 @@ app.use("/productos", productsRouter);
 app.use("/usuarios", usersRouter);
 
 
-// Levantamos el servidor en el puerto 3000
-
-const port =  process.env.PORT || 4000;
-
-app.listen (port, () => {
-    console.log('Servidor corriendo en el puerto ' + port);
-});
 //****************************** */
 // Template Engine
 app.set('view engine', 'ejs');
@@ -71,3 +57,11 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/', mainRoutes);
 app.use('/user', userRoutes);
 //************************************** */
+
+// Levantamos el servidor en el puerto 3000
+
+const port =  process.env.PORT || 3000;
+
+app.listen (port, () => {
+    console.log('Servidor corriendo en el puerto ' + port);
+});
