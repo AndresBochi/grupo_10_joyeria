44b8b3d6b672b55cc5e7639bdd3085edcea92cc9
stefania-tm88 <stefania.tm88@gmail.com>
2023-07-02 03:25:03 +0200

    Cambios session, error rn routes

--- src/app.js
+++ src/app.js
@@ -3,20 +3,27 @@ const express = require('express'); // Importamos modulo express
 const path = require('path'); // Importamos modulo nativo path y lo guardamos en la constante path
 const methodOverride = require('method-override'); // Para poder usar los métodos PUT y DELETE
 const session = require('express-session'); //Requiero el paquete session
+const userLoggedMiddleware = require('./middlewares/userLoggedMiddleware');//************/
+const cookies = require('cookie-parser');//************************** */
 
 // ** express() **
 const app = express(); // Instanciamos Express y lo asignamos a la constante app
+
 
 // ** Template Engine  **
 app.set("view engine", "ejs"); // Configuramos el motor de plantillas
 app.set("views", path.resolve(__dirname, "views")); // Configuramos donde se encuentran las vistas
 
 // ** Middlewares **
+app.use(userLoggedMiddleware);//**************** */
+app.use(cookies());//******************** */
 app.use(express.static(path.join(__dirname, '../public'))); // Habilitamos la carpeta public para ser accedida via http
 app.use(express.urlencoded({ extended: false })); // Para poder leer el body de los formularios
 app.use(express.json());
 app.use(methodOverride('_method')); // Para poder usar los métodos PUT y DELETE
-app.use(session({secret: "Nuestro mensaje secreto"})); //El texto secreto puede ser cualquier texto aleatorio.
+app.use(session({secret: "Nuestro mensaje secreto", //El texto secreto puede ser cualquier texto aleatorio.
+                resave: false,
+	            saveUninitialized: false,}));
 
 // ** Rutas **
 
@@ -24,7 +31,6 @@ app.use(session({secret: "Nuestro mensaje secreto"})); //El texto secreto puede 
 const mainRouter = require("./routes/mainRouter.js")
 const productsRouter = require("./routes/productsRouter.js")
 const usersRouter = require("./routes/usersRouter.js")
-const session = require('express-session')
 
 // Usando los enrutadores importados
 app.use("/", mainRouter);
@@ -34,8 +40,19 @@ app.use("/usuarios", usersRouter);
 
 // Levantamos el servidor en el puerto 3000
 
-const port =  process.env.PORT || 3000;
+const port =  process.env.PORT || 4000;
 
 app.listen (port, () => {
     console.log('Servidor corriendo en el puerto ' + port);
 });
+//****************************** */
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Routers
+const mainRoutes = require('./routes/mainRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+app.use('/', mainRoutes);
+app.use('/user', userRoutes);
+//************************************** */
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -8,18 +8,25 @@ const usersController = require("../controllers/usersController.js");
 // En vez de app.get, utilizamos router.get. Esto va "guardando" en router las distintas rutas, que luego exportamos
 
 //Middlewares Session
+const validations = require('../middlewares/validateRegisterMiddleware');
 const guestMiddleware = require('../middlewares/guestMiddleware');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 // Procesa el pedido get con ruta /register
 routerUsers.get('/registro', guestMiddleware, usersController.register);
-routerUsers.post('/registro', usersController.create);
+// Procesa el pedido post con ruta /register
+routerUsers.post('/registro', uploadFile.single('users'), validations, usersController.processRegister);
+
+
 
 // Procesa el pedido get con ruta /login
 routerUsers.get("/login", usersController.login)
 
 // Perfil de Usuario
 router.get('/profile/', authMiddleware, usersController.profile);
+
+// Proceso de Logout
+router.get('/logout/', usersController.logout);
 
 // Exportamos la variable router ya con todas las rutas "guardadas", que se usará en app.js
 module.exports = routerUsers;
